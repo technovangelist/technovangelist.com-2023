ff0002314bb7893e143b986e5ad781411a3e37e8
Matt Williams <m@technovangelist.com>
2023-08-01 08:24:04 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./_site/**/*.{html,js,md,njk}"],
+  content: [ "./_site/**/*.{html,js,md,njk}", "./src/**/*.{html,js,md,njk}" ],
   theme: {
     extend: {},
   },
